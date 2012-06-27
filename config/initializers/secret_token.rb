7d3cbc4301d1da79ea0699ceb66dac41fd4c9cfc
George Zisiadis <George.Zisiadis@gmail.com>
2012-06-27 14:54:44 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InfiniteStory::Application.config.secret_token = '73ab5db504f8b4d55ec7a68a5690741ba6060c2a0760f12076f6e11758fe9a5d327bf40ac4adff32f43c6056230ccd36109023223713f8b2a1785ee6e06c1771'
